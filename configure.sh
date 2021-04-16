621720f0ac0cbcead4bbed8736b3f75d06cb04e9
hiboy <soapmans@icloud.com>
2021-04-16 23:39:03 +0800

    脚本修改

--- configure.sh
+++ configure.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 
-# Remove temporary directory
-rm -rf /tmp/v2ray
-
 # Download and install V2Ray
 mkdir /tmp/v2ray
-curl -L -H "Cache-Control: no-cache" -o /tmp/v2ray/v2ray.zip https://doc-0s-4g-docs.googleusercontent.com/docs/securesc/qt5bulc20j92ncaug0q6o9rduhvfm61k/53cdf9mp473i7btv7vl7sdqbkb85oj1i/1618587075000/01134674138457490509/01134674138457490509/1YNF9yFtmnOVLWjVIDIhQO2g61Is4MfEP?e=download&authuser=0&nonce=4k5cf89cdkl5q&user=01134674138457490509&hash=j79etacagln0d1goo7ghlfabq5fmjjdc
+curl -L -H "Cache-Control: no-cache" -o /tmp/v2ray/v2ray.zip https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip
 unzip /tmp/v2ray/v2ray.zip -d /tmp/v2ray
 install -m 755 /tmp/v2ray/v2ray /usr/local/bin/v2ray
 install -m 755 /tmp/v2ray/v2ctl /usr/local/bin/v2ctl
